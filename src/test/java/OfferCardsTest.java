ae492c36ef85d866403c15d0fb69a15c962d897b
NataliyaPlat <60834307+NataliyaPlat@users.noreply.github.com>
2020-03-14 09:04:41 -0500

    homework for offer services

---
+++ src/test/java/OfferCardsTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+
+public class OfferListsTest extends BaseTest {
+
+    @Test
+    public void OfferListSizeTest () throws InterruptedException{
+        driver.get("https://testkwidos.tk/offer/search");
+        Thread.sleep(2000);
+        List<WebElement> cardLists = driver.findElements(By.cssSelector("[class*='card-shape']"));
+        Assert.assertEquals(cardLists.size(),10);
+    }
+    @Test
+    public void FilterByServiceTypeTest() throws InterruptedException{
+        driver.get("https://testkwidos.tk/offer/search");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//label[contains(text(), 'Service Type')]/../p-multiselect")).click();
+        driver.findElement(By.xpath("//label[contains(text(), '3D Capture')]")).click();
+        Thread.sleep(2000);
+        List<WebElement> serviceTypeCards = driver.findElements(By.cssSelector("[class*='service-type-flex']"));
+        for (WebElement element: serviceTypeCards){
+            System.out.println(element.getText());
+            Assert.assertEquals(element.getText(), "3D Capture");
+        }
+    }
+}
